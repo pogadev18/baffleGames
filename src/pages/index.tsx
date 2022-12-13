ce81c4fd25f41c23f6600c31f8a0b0c9e42ecb2e
pogadev <pogadev18@gmail.com>
2022-12-13 19:30:13 +0000

    firebase client and admin configuration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+import { ConnectWallet } from "@thirdweb-dev/react";
+import type { NextPage } from "next";
+
+import initializeFirebaseClient from "@/root/lib/initFirebase";
+
+const Home: NextPage = () => {
+  const {auth, db} = initializeFirebaseClient()
+
   return (
-    <section>
+    <section className='w-1/2 text-center m-auto py-10'>
       <h1 className='text-white font-bold text-7xl'>Hello</h1>
+      <div className='mt-10'>
+
+        <ConnectWallet colorMode='dark'/>
+      </div>
     </section>
   )
 }
+
+export default Home;
